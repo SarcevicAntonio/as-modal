c37e444c7bf04c760e9b060c5e961ec570a389b7
Antonio Sarcevic <sarcevic@westphalia-datalab.com>
2021-09-29 10:18:36 +0200

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
+import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,8 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: staticAdapter()
 	}
 };
 
