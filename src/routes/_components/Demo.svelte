e831525ef6c30ffebafb11b5dee7f290802f1210
Antonio Sarcevic <sarcevic@westphalia-datalab.com>
2021-09-29 10:17:30 +0200

    added mdsvex support

---
+++ src/routes/_components/Demo.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Modal from '$lib/Modal.svelte';
+
+	let deleteModalOpen = false;
+
+	function deleteEntry() {
+		deleteModalOpen = false;
+		alert('call delete function');
+	}
+</script>
+
+
+<h2 id="demo">Demo</h2>
+
+<section>
+	<span>Some Entry in a List or something</span>
+
+	<Modal bind:open={deleteModalOpen}>
+		<svelte:fragment slot="triggerLabel">Delete</svelte:fragment>
+
+		<h2>Are you sure you want to delete the entry?</h2>
+		<p>This action can not be reversed.</p>
+
+		<svelte:fragment slot="modalActions">
+			<button on:click={() => (deleteModalOpen = false)}> No </button>
+			<button on:click={deleteEntry}>Yes </button>
+		</svelte:fragment>
+	</Modal>
+</section>
+
+<style>
+	span {
+		margin-right: 1em;
+	}
+	h2 {
+		margin-top: 0;
+	}
+	:global(button) {
+		flex-grow: 1;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 0;
+		gap: 0.5em;
+	}
+</style>
---
+++ src/routes/index.md
@@ -0,0 +1,22 @@
+<script>
+  import Demo from "./_components/Demo.svelte";
+  import Docs from "./_components/Docs.md";
+
+</script>
+
+# AS Modal
+
+Simple and customizable modal dialog for Svelte
+
+_docs under construction_
+
+## Links
+
+- [Open quick start in Svelte REPL](https://svelte.dev/repl/ac1ac6289ab948b488fe2f17d122aaac?version=3.42.6)
+- [View a demo](#demo)
+- [Vist package on npm](https://www.npmjs.com/package/as-modal)
+- [Visit project repo on GitHub](https://github.com/SarcevicAntonio/as-modal).
+
+<Demo />
+
+<Docs />
